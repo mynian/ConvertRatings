da7d82b9511f7df01924381f2ba1a0939240462d
Mynian <mynian@gmail.com>
2023-02-17 21:22:06 -0500

    more gem work

--- ConvertRatings.lua
+++ ConvertRatings.lua
@@ -2304,33 +2304,6 @@ local function getItemIdFromTooltip(self)
 	end	
 end
 
---Code for printing tables to the chat window for debug purposes
---[[local function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  local toprint = string.rep(" ", indent) .. "{\r\n"
-  indent = indent + 2 
-  for k, v in pairs(tbl) do
-    toprint = toprint .. string.rep(" ", indent)
-    if (type(k) == "number") then
-      toprint = toprint .. "[" .. k .. "] = "
-    elseif (type(k) == "string") then
-      toprint = toprint  .. k ..  "= "   
-    end
-    if (type(v) == "number") then
-      toprint = toprint .. v .. ",\r\n"
-    elseif (type(v) == "string") then
-      toprint = toprint .. "\"" .. v .. "\",\r\n"
-    elseif (type(v) == "table") then
-      toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
-    else
-      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
-    end
-  end
-  toprint = toprint .. string.rep(" ", indent-2) .. "}"
-  return toprint
-end
-]]
-
 --Function for getting gem info
 --This method successfully gets the itemid of the gems that are equipped, what to do from there ..... ??????
 --[[local function getgeminfo(self)
@@ -2347,7 +2320,10 @@ end
      		return;
 	end
 	
-	local _, itemID, enchantID, gem1, gem2, gem3, gem4 = strsplit(":", strmatch(itemLink, "|H(.-)|h"))	
+	local _, itemID, enchantID, gem1, gem2, gem3, gem4 = strsplit(":", strmatch(itemLink, "|H(.-)|h"))
+	
+	--This gives me a table of the data used to genereate the tooltip for the gem
+	local gem1info = C_Tooltipinfo.GetItemByID(gem1)
 	
 end
 ]]
